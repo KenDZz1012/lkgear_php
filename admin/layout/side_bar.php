ce6e66f0a628b217aebb4be7acf028aa944d88c3
ntk <kienteo1012@gmail.com>
2021-12-29 17:56:21 +0700

    first commit

---
+++ admin/layout/side_bar.php
@@ -0,0 +1,98 @@
+<div class="nk-sidebar">
+    <div class="nk-nav-scroll">
+        <ul class="metismenu" id="menu">
+            <li class="nav-label">Dashboard</li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/index.php" aria-expanded="false">
+                    <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                </a>
+                <!-- <ul aria-expanded="false">
+                    <li><a href="">Home 1</a></li>
+                    
+                </ul> -->
+            </li>
+            <li class="mega-menu mega-menu-sm">
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Layouts</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="<?php echo $base_url ?>admin/fe/index.php">Danh Sách layout</a></li>
+                    
+                </ul>
+            </li>
+           
+            <li class="mega-menu mega-menu-sm">
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Account</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="<?php echo $base_url ?>admin/account">Danh Sách Account</a></li>
+                    
+                </ul>
+            </li>
+            <li class="mega-menu mega-menu-sm">
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-globe-alt menu-icon"></i><span class="nav-text">Hóa Đơn</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="<?php echo $base_url ?>admin/bill">Danh Sách Hóa Đơn</a></li>
+                    
+                </ul>
+            </li>
+            <li class="nav-label">Apps</li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/category/index.php" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Category</span>
+                </a>
+                
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/brand/index.php" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Brand</span>
+                </a>
+                
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/product/index.php" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">Product</span>
+                </a>
+                
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/prdchill/index.php" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">PrdChill</span>
+                </a>
+                
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/descprdchill/index.php" aria-expanded="false">
+                    <i class="icon-envelope menu-icon"></i> <span class="nav-text">descprdchill</span>
+                </a>
+                
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/comment/index.php" aria-expanded="false">
+                    <i class="icon-screen-tablet menu-icon"></i><span class="nav-text">Comment</span>
+                </a>
+               
+            </li>
+            <li>
+                <a class="has-arrow" href="<?php echo $base_url ?>admin/user/index.php" aria-expanded="false">
+                    <i class="icon-screen-tablet menu-icon"></i><span class="nav-text">User</span>
+                </a>
+               
+            </li>
+            
+            <li class="nav-label">Pages</li>
+            <li>
+                <a class="has-arrow" href="javascript:void()" aria-expanded="false">
+                    <i class="icon-notebook menu-icon"></i><span class="nav-text">Pages</span>
+                </a>
+                <ul aria-expanded="false">
+                    <li><a href="<?php echo $base_url ?>">Web</a></li>
+                    
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
